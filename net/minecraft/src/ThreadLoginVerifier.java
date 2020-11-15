9f179d544c9baf3a28607bbf18bb31f849d6b64a
Luminoso <Anishjindal08@gmail.com>
2020-11-15 08:34:48 -0600

    Defaulting to Mine online backend

--- net/minecraft/src/ThreadLoginVerifier.java
+++ net/minecraft/src/ThreadLoginVerifier.java
@@ -4,7 +4,9 @@ package net.minecraft.src;
 // Decompiler options: packimports(3) braces deadcode 
 
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.logging.Logger;
 
 class ThreadLoginVerifier extends Thread
 {
@@ -17,21 +19,46 @@ class ThreadLoginVerifier extends Thread
 
     public void run()
     {
+        PropertyManager propertyManager = new PropertyManager(new File("server.properties"));
+
         try
         {
-            String s = NetLoginHandler.getServerId(loginHandler);
-            URL url = new URL((new StringBuilder()).append("http://www.minecraft.net/game/checkserver.jsp?user=").append(loginPacket.username).append("&serverId=").append(s).toString());
-            BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(url.openStream()));
-            String s1 = bufferedreader.readLine();
-            bufferedreader.close();
-            System.out.println((new StringBuilder()).append("THE REPLY IS ").append(s1).toString());
-            if(s1.equals("YES"))
-            {
-                NetLoginHandler.setLoginPacket(loginHandler, loginPacket);
-            } else
-            {
-                loginHandler.kickUser("Failed to verify username!");
+            String serverId = NetLoginHandler.getServerId(loginHandler);
+            URL url = null;
+            if(propertyManager.getBooleanProperty("usemineonlinebackend", true)){
+                HttpURLConnection connection;
+                Logger logger = Logger.getLogger("Minecraft");
+                logger.info("Getting URL: "+"https://sessionserver.mojang.com" + "/session/minecraft/hasJoined?username=" + loginPacket.username + "&serverId=" + serverId );
+                url = new URL( "https://sessionserver.mojang.com" + "/session/minecraft/hasJoined?username=" + loginPacket.username + "&serverId=" + serverId);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setDoInput(true);
+                connection.setDoOutput(false);
+
+                connection.connect();
+                if(connection.getResponseCode() == 200){
+                    NetLoginHandler.setLoginPacket(loginHandler, loginPacket);
+                }
+                else{
+                    loginHandler.kickUser("Failed to verify username!");
+                }
             }
+            else {
+                url = new URL((new StringBuilder()).append("http://www.minecraft.net/game/checkserver.jsp?user=").append(loginPacket.username).append("&serverId=").append(serverId).toString());
+
+                BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(url.openStream()));
+                String s1 = bufferedreader.readLine();
+                bufferedreader.close();
+                System.out.println((new StringBuilder()).append("THE REPLY IS ").append(s1).toString());
+                if(s1.equals("YES"))
+                {
+                    NetLoginHandler.setLoginPacket(loginHandler, loginPacket);
+                } else
+                {
+                    loginHandler.kickUser("Failed to verify username!");
+                }
+            }
+
         }
         catch(Exception exception)
         {
