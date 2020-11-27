f404ea29e679392c99d3dc3b158f8155a0bc0fec
Anishjindal08@gmail.com <anishjindal08@gmail.com>
2020-11-27 07:33:30 -0600

    Updating  examples

--- res/example_plugin.lua
+++ res/example_plugin.lua
@@ -4,3 +4,7 @@ function OnServerInit()
     minecraft_server:log("This is an example plugin hooking into the minecraft server class")
     return "HOOK_COMPLETE" -- Hooks will eventually have return states. For now this is a placeholder.
 end
+
+function OnBlockPlace()
+
+end
